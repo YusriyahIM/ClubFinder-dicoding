c6bc130ef71628a2b405b215882d4739af0edfc2
Yusriyah IM <yusriyahisna24@gmail.com>
2021-10-05 19:28:57 +0700

    adjust Template literals, Destructuring Object, Arrow Function, Class, Promise, Module, Custom Element, and Shadow DOM

--- src/script/data/data-source.js
+++ src/script/data/data-source.js
@@ -1,16 +1,46 @@
-function DataSource(onSuccess, onFailed) {
-  this.onSuccess = onSuccess;
-  this.onFailed = onFailed;
+import clubs from "./clubs.js";
+// function DataSource(onSuccess, onFailed) {
+//   this.onSuccess = onSuccess;
+//   this.onFailed = onFailed;
+// }
+
+// DataSource.prototype.searchClub = function (keyword) {
+//   //   const filteredClubs = clubs.filter(function (club) {
+//   //     return club.name.toUpperCase().includes(keyword.toUpperCase());
+//   //   });
+//   const filteredClubs = clubs.filter((club) => club.name.toUpperCase().includes(keyword.toUpperCase()));
+
+//   if (filteredClubs.length) {
+//     this.onSuccess(filteredClubs);
+//   } else {
+//     // this.onFailed(keyword + " is not found");
+//     this.onFailed(`${keyword} is not found`);
+//   }
+// };
+// Mengubah menjadi Class seperti dibawah
+class DataSource {
+  //   constructor(onSuccess, onFailed) {
+  //     this.onSuccess = onSuccess;
+  //     this.onFailed = onFailed;
+  //   }
+  static searchClub(keyword) {
+    // const filteredClubs = clubs.filter((club) => club.name.toUpperCase().includes(keyword.toUpperCase()));
+    // if (filteredClubs.length) {
+    //   this.onSuccess(filteredClubs);
+    // } else {
+    //   this.onFailed(`${keyword} is not found"`);
+    // }
+    return new Promise((resolve, reject) => {
+      const filteredClubs = clubs.filter((club) => club.name.toUpperCase().includes(keyword.toUpperCase()));
+      if (filteredClubs.length) {
+        // this.onSuccess(filteredClubs);
+        resolve(filteredClubs);
+      } else {
+        // this.onFailed(`${keyword} is not found"`);
+        reject(`${keyword} is not found`);
+      }
+    });
+  }
 }
 
-DataSource.prototype.searchClub = function (keyword) {
-  const filteredClubs = clubs.filter(function (club) {
-    return club.name.toUpperCase().includes(keyword.toUpperCase());
-  });
-
-  if (filteredClubs.length) {
-    this.onSuccess(filteredClubs);
-  } else {
-    this.onFailed(`${keyword} is not found`);
-  }
-};
+export default DataSource;
--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,41 +1,72 @@
+import "../component/club-list.js";
 import "../component/search-bar.js";
+import DataSource from "../data/data-source.js";
 
-const main = function () {
-  // const searchElement = document.querySelector("#searchElement");
+const main = () => {
+  //   const searchElement = document.querySelector("#searchElement");
   const searchElement = document.querySelector("search-bar");
-  // const buttonSearchElement = document.querySelector("#searchButtonElement");
-  const clubListElement = document.querySelector("#clubList");
+  //   const buttonSearchElement = document.querySelector("#searchButtonElement");
 
-  const onButtonSearchClicked = function () {
-    const dataSource = new DataSource(renderResult, fallbackResult);
-    dataSource.searchClub(searchElement.value);
+  //   const clubListElement = document.querySelector("#clubList");
+  const clubListElement = document.querySelector("club-list");
+
+  //   const onButtonSearchClicked = () => {
+  //     const dataSource = new DataSource(renderResult, fallbackResult);
+  //     dataSource.searchClub(searchElement.value);
+  //   };
+  //   Dibuat menjadi seperti dibawah
+
+  //   const onButtonSearchClicked = () => {
+  //     DataSource.searchClub(searchElement.value).then(renderResult).catch(fallbackResult);
+  //   };
+  // Kalau pake async/await cide seperti dibawah
+  const onButtonSearchClicked = async () => {
+    try {
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
+    } catch (message) {
+      fallbackResult(message);
+    }
   };
 
-  const renderResult = function (results) {
-    clubListElement.innerHTML = "";
-    results.forEach(function (club) {
-      const name = club.name;
-      const fanArt = club.fanArt;
-      const description = club.description;
-
-      const clubElement = document.createElement("div");
-      clubElement.setAttribute("class", "club");
-
-      clubElement.innerHTML = `
-        <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
-        <div class="club-info">
-            <h2>${name}</h2>
-            <p>${description}</p>
-        </div>`;
-      clubListElement.appendChild(clubElement);
-    });
+  const renderResult = (results) => {
+    clubListElement.clubs = results;
   };
 
-  const fallbackResult = function (message) {
-    clubListElement.innerHTML = "";
-    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+  //   const renderResult = (results) => {
+  //     clubListElement.innerHTML = "";
+  //     results.forEach((club) => {
+  //   const name = club.name;
+  //   const fanArt = club.fanArt;
+  //   const description = club.description;
+  //   const { name, fanArt, description } = club;
+
+  //   const clubElement = document.createElement("div");
+  //   clubElement.setAttribute("class", "club");
+
+  //   clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' + '<div class="club-info">\n' + "<h2>" + name + "</h2>\n" + "<p>" + description + "</p>" + "</div>";
+
+  //       clubElement.innerHTML = `
+  //         <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+  //         <div class="club-info">
+  //         <h2>${name}</h2>
+  //         <p>${description}</p>
+  //         </div>`;
+
+  //       clubListElement.appendChild(clubElement);
+  //     });
+  //   };
+
+  const fallbackResult = (message) => {
+    clubListElement.renderError(message);
+
+    // clubListElement.innerHTML = "";
+    // clubListElement.innerHTML += '<h2 class="placeholder">' + message + "</h2>";
+
+    // clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
-  // buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  //   buttonSearchElement.addEventListener("click", onButtonSearchClicked);
   searchElement.clickEvent = onButtonSearchClicked;
 };
+export default main;
